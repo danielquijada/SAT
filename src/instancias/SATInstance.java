d46f57c8bde349a10b3a83a07804ea7fe854eaa5
Daniel E. Quijada Diaz <daniventf@gmail.com>
2015-12-05 15:15:11 +0000

    Commit Inicial

---
+++ src/instancias/SATInstance.java
@@ -0,0 +1,27 @@
+/**
+ * Final.TransformacionPolinomial.instancias.SATInstance.java
+ * Practica Final. Transformación Polinomial entre SAT y 3SAT.
+ *
+ * Asignatura: Complejidad Computacional
+ * Universidad de La Laguna.
+ * Curso: 2015-2016
+ *
+ * Autores: 
+ * 	Daniel E. Quijada Díaz
+ * 		Contacto: daniventf@gmail.com
+ * 	José Carlos Rodríguez Cortés
+ * 		Contacto: joseka1234@gmail.com
+ * 	Manuel Alejandro Rodríguez Santana
+ * 		Contacto: manwelanza@gmail.com
+ * 	Mauricio Manuel Cavalleri Sergent
+ * 		Contacto: maurimanuel92@gmail.com
+ */
+package instancias;
+
+
+/**
+ * TODO Descripción de la clase.
+ */
+public class SATInstance {
+
+}
---
+++ src/modelo/Variable.java
@@ -0,0 +1,27 @@
+/**
+ * Final.TransformacionPolinomial.modelo.Variable.java
+ * Practica Final. Transformación Polinomial entre SAT y 3SAT.
+ *
+ * Asignatura: Complejidad Computacional
+ * Universidad de La Laguna.
+ * Curso: 2015-2016
+ *
+ * Autores: 
+ * 	Daniel E. Quijada Díaz
+ * 		Contacto: daniventf@gmail.com
+ * 	José Carlos Rodríguez Cortés
+ * 		Contacto: joseka1234@gmail.com
+ * 	Manuel Alejandro Rodríguez Santana
+ * 		Contacto: manwelanza@gmail.com
+ * 	Mauricio Manuel Cavalleri Sergent
+ * 		Contacto: maurimanuel92@gmail.com
+ */
+package modelo;
+
+
+/**
+ * TODO Descripción de la clase.
+ */
+public class Variable {
+
+}
